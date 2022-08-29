728b7aca797fad93fd0d946bc97d83cca200e9b2
Albert Sobreo <43039667+albert-sobreo@users.noreply.github.com>
2022-08-30 03:23:57 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,6 +3,6 @@ type LICENSE
 SET /P AREYOUSURE=Do you agree with the terms of the license? (y/[n])?
 IF /I "%AREYOUSURE%" NEQ "y" GOTO END
 python-3.10.6-amd64.exe InstallAllUsers=1 PrependPath=1 Include_test=0 ^
-&& python -m ensurepip && python -m pip install -r backend\requirements.txt && node-v16.17.0-x64.msi && cd frontend && npm install
+&& python -m ensurepip && python -m pip install -r backend\requirements.txt && node-v16.17.0-x64.msi
 
 :END
